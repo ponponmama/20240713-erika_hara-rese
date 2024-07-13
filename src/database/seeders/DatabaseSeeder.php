8ec61a43715abd2948363ac186ac6fb4559e0e44
ponponmama <buti1024@gmail.com>
2024-07-13 19:02:43 +0900

    reseアップデート

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\ShopsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(ShopsTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
