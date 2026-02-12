864865323ed6642956df2247a989235c8d47e42b
ponponmama <buti1024@gmail.com>
2026-02-12 23:22:58 +0900

    indexページ検索フォーム修正

--- src/resources/views/shop_manager/manage-shop.blade.php
+++ src/resources/views/shop_manager/manage-shop.blade.php
@@ -10,91 +10,83 @@
 @endsection
 
 @section('content')
-    <div class="container manage_shop_container">
+    <p class="session-messages">
+        @include('custom_components.session-messages')
+    </p>
+    <form action="{{ route('shop_manager.update', $shop->id) }}" method="POST" enctype="multipart/form-data" class="manage_form">
+        @csrf
+        @method('PUT')
         <h2 class="content-section-title">店舗情報</h2>
-        <p class="session-messages">
-            @include('custom_components.session-messages')
-        </p>
-        <form action="{{ route('shop_manager.update', $shop->id) }}" method="POST" enctype="multipart/form-data"
-            class="manage_form">
-            @csrf
-            @method('PUT')
-            <div class="form-group">
-                <span class="label-title">
-                    <img src="{{ asset('images/shop.png') }}" alt="" class="icon-img">
-                    店舗名
-                </span>
-                <span id="shop_name" class="data-entry shop-name-entry" name="shop_name">{{ $shop->shop_name }}</span>
-            </div>
-            <div class="form-group">
-                <label for="description" class="label-title description-title">
-                    <img src="{{ asset('images/description.png') }}" alt="" class="icon-img">
-                    店舗紹介
-                </label>
-                <textarea id="description" name="description" class="data-entry description_text">{{ $shop->description }}</textarea>
-            </div>
-            <p class="form__error">
-                @error('description')
-                    {{ $message }}
-                @enderror
-            </p>
-            <div class="form-group">
-                <span class="label-title business_hours"><img src="{{ asset('images/clock.svg') }}" alt=""
-                        class="icon-img">営業時間</span>
-            </div>
-            <div class="form-group">
-                <label for="open_time" class="label-title">
-                    オープン
-                </label>
-                <input type="time" id="open_time" name="open_time"
-                    value="{{ \Carbon\Carbon::parse($shop->open_time)->format('H:i') }}" class="data-entry input_time">
-            </div>
-            <p class="form__error">
-                @error('open_time')
-                    {{ $message }}
-                @enderror
-            </p>
-            <div class="form-group">
-                <label for="close_time" class="label-title">
-                    クローズ
-                </label>
-                <input type="time" id="close_time" name="close_time"
-                    value="{{ \Carbon\Carbon::parse($shop->close_time)->format('H:i') }}" class="data-entry input_time">
-            </div>
-            <p class="form__error">
-                @error('close_time')
-                    {{ $message }}
-                @enderror
-            </p>
-            <div class="form-group">
-                <label for="image" class="label-title">
-                    <img src="{{ asset('images/img.png') }}" alt="" class="icon-img">
-                    写真
-                </label>
-                <input type="file" id="image" name="image" class="input_image" accept="image/*">
-                <label for="image" class="data-entry custom-file-upload">
-                    写真を選択
-                </label>
-            </div>
-            <p class="form__error">
-                @error('image')
-                    {{ $message }}
-                @enderror
-            </p>
-            <span id="file-name" class="file-name"></span>
-            <span class="preview-image-container"></span>
-            <div class="up_date_button_container">
-                <button type="submit" class="button up_date_button">更新する</button>
-            </div>
-        </form>
-        <p class="confirm-text">確認はこちらから</p>
-        <div class="confirm-button-container" id="confirm-button-container">
-            <button type="button" class="button confirm-button" id="confirm-button">更新を確認</button>
-            <a href="{{ route('shops.index', ['from_admin' => 'true', 'shop_id' => $shop->id]) }}"
-                class="view-shop-link link">
-                店舗一覧ページで確認する
-            </a>
+        <div class="form-group">
+            <span class="label-title">
+                <img src="{{ asset('images/shop.png') }}" alt="" class="icon-img">
+                店舗名
+            </span>
+            <span id="shop_name" class="data-entry shop-name-entry" name="shop_name">{{ $shop->shop_name }}</span>
         </div>
+        <div class="form-group">
+            <label for="description" class="label-title description-title">
+                <img src="{{ asset('images/description.png') }}" alt="" class="icon-img">
+                店舗紹介
+            </label>
+            <textarea id="description" name="description" class="data-entry description_text">{{ $shop->description }}</textarea>
+        </div>
+        <p class="form__error">
+            @error('description')
+                {{ $message }}
+            @enderror
+        </p>
+        <div class="form-group">
+            <label for="image" class="label-title">
+                <img src="{{ asset('images/img.png') }}" alt="" class="icon-img">
+                写真
+            </label>
+            <input type="file" id="image" name="image" class="input_image" accept="image/*">
+            <label for="image" class="data-entry custom-file-upload">
+                写真を選択
+            </label>
+        </div>
+        <p class="form__error">
+            @error('image')
+                {{ $message }}
+            @enderror
+        </p>
+        <span class="preview-image-container hide"></span>
+        <span id="file-name" class="file-name"></span>
+        <div class="form-group">
+            <label for="open_time" class="label-title business_hours">
+                <img src="{{ asset('images/clock.svg') }}" alt="" class="icon-img">
+                オープン
+            </label>
+            <input type="time" id="open_time" name="open_time" value="{{ \Carbon\Carbon::parse($shop->open_time)->format('H:i') }}" class="data-entry input_time">
+        </div>
+        <p class="form__error">
+            @error('open_time')
+                {{ $message }}
+            @enderror
+        </p>
+        <div class="form-group">
+            <label for="close_time" class="label-title business_hours">
+                <img src="{{ asset('images/clock.svg') }}" alt="" class="icon-img">
+                クローズ
+            </label>
+            <input type="time" id="close_time" name="close_time" value="{{ \Carbon\Carbon::parse($shop->close_time)->format('H:i') }}" class="data-entry input_time">
+        </div>
+        <p class="form__error">
+            @error('close_time')
+                {{ $message }}
+            @enderror
+        </p>
+        <div class="up_date_button_container">
+            <button type="submit" class="button up_date_button">更新する</button>
+        </div>
+    </form>
+    <p class="confirm-text">確認はこちらから</p>
+    <div class="confirm-button-container" id="confirm-button-container">
+        <button type="button" class="button confirm-button" id="confirm-button">更新を確認</button>
+        <a href="{{ route('shops.index', ['from_admin' => 'true', 'shop_id' => $shop->id]) }}" class="view-shop-link link">
+            店舗一覧ページで確認する
+        </a>
     </div>
 
     <!-- 更新確認モーダル -->
